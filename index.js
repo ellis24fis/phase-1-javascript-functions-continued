6aec1a285e67960d226c9add51ec29ee0826e14d
Valerie Ellis <valerie.ellis@flatironschool.com>
2024-04-03 20:59:18 -0400

    adjective is a confusing thing to say, I would probably call it job title. same with visual flair and highlight, I would probably say something like wrap with special characters

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+   return (`This Saturday, I want to ${activity}!`);
+  }
+  saturdayFun();
+  saturdayFun(dotDotDot);
+  
+
+function mondayWork(place = "go to the office") {
+    return (`This Monday, I will ${place}.`);
+   }
+   mondayWork();
+   mondayWork("work from home");
+
+function wrapAdjective(character = "*"){
+   return function inner(adjective = "special"){
+    return (`You are ${character}${adjective}${character}!`)
+   }
+     
+}
